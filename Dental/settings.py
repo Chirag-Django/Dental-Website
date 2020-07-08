fad580ead4e76d106e543c40c813d18929606f94
Chirag Dave <chirag.django@gmail.com>
2020-07-08 17:36:03 +0530

    Initial Commit

--- Dental/settings.py
+++ Dental/settings.py
@@ -130,8 +130,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'chirag.django@gmail.com'
-EMAIL_HOST_PASSWORD = 'ckdave@123'
+EMAIL_HOST_USER = 'email@gmail.com'
+EMAIL_HOST_PASSWORD = 'password'
 
 
 # EMAIL_HOST = 'localhost'
